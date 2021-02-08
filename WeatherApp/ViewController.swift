0be04a1d6441ca99442e966e66728fb6e5c573d1
Anshu Vij <anshu.vij@sustlabs.com>
2021-02-08 19:34:58 +0530

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Mobile Apps Team on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
